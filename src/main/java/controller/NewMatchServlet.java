93d54a4cca5efbca7e4dff2bf11e10535ded0476
J-Bakuli <julia.bakuli@yandex.ru>
2026-05-21 14:23:08 +0300

    Add NewMatchServlet

---
+++ src/main/java/controller/NewMatchServlet.java
@@ -0,0 +1,51 @@
+package controller;
+
+import dao.H2PlayerDao;
+import dao.OngoingMatchDao;
+import dao.PlayerDao;
+import db.AppLifecycleListener;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import service.NewMatchService;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@WebServlet({"/new-match"})
+@Slf4j
+public class NewMatchServlet extends HttpServlet {
+    private NewMatchService newMatchService;
+
+    @Override
+    public void init() throws ServletException {
+        PlayerDao playerDao = new H2PlayerDao();
+        OngoingMatchDao ongoingMatchDao = (OngoingMatchDao) getServletContext().getAttribute(AppLifecycleListener.ONGOING_MATCH_DAO_ATTR);
+        if (ongoingMatchDao == null) {
+            throw new ServletException("OngoingMatchDao is not initialized in ServletContext");
+        }
+        newMatchService = new NewMatchService(playerDao, ongoingMatchDao);
+    }
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        log.info("GET /new-match");
+        req.getRequestDispatcher("/WEB-INF/views/new-match.jsp").forward(req, resp);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String player1Name = req.getParameter("player1Name");
+        String player2Name = req.getParameter("player2Name");
+
+        log.info("POST /new-match - received: player1Name='{}', player2Name='{}'",
+                player1Name != null ? player1Name : "null",
+                player2Name != null ? player2Name : "null");
+
+        UUID matchId = newMatchService.startNewMatch(player1Name, player2Name);
+        resp.sendRedirect(req.getContextPath() + "/match-score?uuid=" + matchId);
+    }
+}
